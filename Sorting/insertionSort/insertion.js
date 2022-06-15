8e16586bad04bdd0311aaecc5f2a42a68abbfdc4
Madhusudan Pareek <85008599+MadMods96@users.noreply.github.com>
2022-06-15 18:55:56 +0530

    output of insertion sort
    [ 1, 2, 3, 4, 5, 6 ]

--- Sorting/insertionSort/insertion.js
+++ Sorting/insertionSort/insertion.js
@@ -1,1 +1,17 @@
+function insertionSort(arr, n){
+    let current,i,j;
+    for(i = 0; i<n; i++){
+        current = arr[i];
+        j = i-1;
+        // this loop will contineaously run till it finds the index where current element should be placed
+        while(arr[j]>current && j>=0){
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = current;
+    }
+    return arr;
+}
 
+
+console.log(insertionSort([3,4,6,5,2,1], 6));
