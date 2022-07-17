e2eee2a789fbe4b77583c21d40b94cbaeee9653d
Madhusudan Pareek <85008599+MadMods96@users.noreply.github.com>
2022-07-17 13:35:09 +0530

    Update factorial.js

--- Recursion/factorial.js
+++ Recursion/factorial.js
@@ -12,3 +12,34 @@ function find(n){
 }
 console.log(find(5));
 //output : 120
+
+
+// this is an example of calculating the factorial OF a number with recursive function
+
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("What the number", function (num) {
+  console.log(main(num));
+  rl.close();
+});
+
+rl.on("close", function () {
+  process.exit(0);
+});
+
+function main(num) {
+  return factorial(num);
+}
+
+function factorial(n) {
+  if (n == 0 || n == 1) {
+    return 1;
+  }
+
+  return n * factorial(n - 1);
+}
+
