03c4733dddb1202dd1fa4cd397500b5a207a3469
Madhusudan Pareek <85008599+MadMods96@users.noreply.github.com>
2022-08-04 13:29:05 +0530

    Create powerOfTwo.js

---
+++ other/powerOfTwo.js
@@ -0,0 +1,12 @@
+//Leetcode : 231. Power of Two
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function(n) {
+    if(n <=0) return false;
+
+    return (n & (n-1))===0;
+    //bitwise AND
+};
